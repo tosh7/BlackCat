a2d6260316906760152cfb3482cdd0f86754633d
tosh7 <zlia.6.lj.425@gmail.com>
2021-06-09 21:29:50 +0900

    Initial Commit

---
+++ BlackCat/BlackCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackCatApp.swift
+//  BlackCat
+//
+//  Created by Satoshi Komatsu on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct BlackCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
