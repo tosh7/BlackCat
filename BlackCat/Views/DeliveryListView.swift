1c6f16062f402594a325807949700fd47c402311
tosh7 <zlia.6.lj.425@gmail.com>
2021-06-10 18:51:44 +0900

    Added ScrollView

--- BlackCat/Views/DeliveryListView.swift
+++ BlackCat/Views/DeliveryListView.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 
 struct DeliveryListView: View {
+
+    var columns: [GridItem] = Array(repeating: .init(.fixed(50)), count: 5)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView(.vertical) {
+                LazyVGrid(columns: columns,
+                          alignment: .center,
+                          spacing: 8) {
+                    ForEach((1...1000), id: \.self) { num in
+                        ZStack {
+                            Rectangle().foregroundColor(.accentColor)
+                            Text("\(num)").foregroundColor(.white)
+                        }
+                    }
+                }
+            }
     }
 }
 
