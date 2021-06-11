ae4f40fabee61e656a5e75770bfa6b4cfbc1a6cb
tosh7 <zlia.6.lj.425@gmail.com>
2021-06-11 18:09:56 +0900

    Added another set of URLSession

--- BlackCat/Models/APIProtcols/ApiClient.swift
+++ BlackCat/Models/APIProtcols/ApiClient.swift
@@ -13,13 +13,14 @@ struct ApiClident {
         guard let url = URL(string: self.basePath) else { return }
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         let json: [String : Any] = [
             "number00": 1,
             "number01": "429636181995"
         ]
         request.httpBody = try! NSKeyedArchiver.archivedData(withRootObject: json, requiringSecureCoding: false)
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            print(error)
+//            print(error)
             print(response)
         }
         task.resume()
