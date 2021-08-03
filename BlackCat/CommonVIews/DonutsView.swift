ba94924e2876c698efa8b200f4a6c72b48cca0f6
tosh7 <zlia.6.lj.425@gmail.com>
2021-08-04 02:59:36 +0900

    Made Donuts view

---
+++ BlackCat/CommonVIews/DonutsView.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+struct DonutsView: View {
+
+    private let deliveryStatusType: DeliveryStatusType
+    private var trimValue: CGFloat {
+        switch deliveryStatusType {
+        case .received:
+            return 0.25
+        case .sended:
+            return 0.5
+        case .delivering:
+            return 0.75
+        case .delivered:
+            return 1
+        }
+    }
+
+    init(deliveryStatusType: DeliveryStatusType) {
+        self.deliveryStatusType = deliveryStatusType
+    }
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(Color.blue, lineWidth: 20)
+                .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+
+            Circle()
+                .trim(from: 0.0, to: trimValue)
+                .rotation(Angle(degrees: 270.0))
+                .stroke(Color.red, lineWidth: 20)
+                .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct DonutsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DonutsView(deliveryStatusType: .delivering)
+    }
+}
--- BlackCat/CommonVIews/LuggageItemGrid.swift
+++ BlackCat/CommonVIews/LuggageItemGrid.swift
@@ -1,19 +1,41 @@
 import SwiftUI
 
 struct LuggageItemGrid: View {
-    let state: String
+    let deliveryStatus: DeliveryStatus
 
-    init(state: String) {
-        self.state = state
+    init(deliveryStatus: DeliveryStatus) {
+        self.deliveryStatus = deliveryStatus
     }
 
     var body: some View {
-           Text(state)
+        ZStack {
+            deliveryStatus.deliveryStatus?.color
+                .edgesIgnoringSafeArea(.all)
+
+            DonutsView(deliveryStatusType: deliveryStatus.deliveryStatus ?? .sended)
+
+            VStack {
+                Text(deliveryStatus.status)
+                    .foregroundColor(.white)
+                Text(deliveryStatus.shopName)
+                    .foregroundColor(.white)
+                HStack {
+                    Text(deliveryStatus.date)
+                        .foregroundColor(.white)
+                    Text(deliveryStatus.time)
+                        .foregroundColor(.white)
+                }
+            }
+        }
     }
 }
 
 struct LuggageItemGrid_Previews: PreviewProvider {
     static var previews: some View {
-        LuggageItemGrid(state: "配達中")
+        LuggageItemGrid(deliveryStatus: DeliveryStatus(status: "輸送中",
+                                              date: "6/13",
+                                              time: "12:41",
+                                              shopName: "羽田クロノゲートベース",
+                                              shopID: "032990"))
     }
 }
