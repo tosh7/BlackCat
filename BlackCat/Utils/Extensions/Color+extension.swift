f7d6fe6bde9fc9205377f03b694dd3481c17495e
tosh7 <zlia.6.lj.425@gmail.com>
2021-08-06 00:01:15 +0900

    Switch Int32 to Int64 becasuse scanHexInt64 is deprecated

--- BlackCat/Utils/Extensions/Color+extension.swift
+++ BlackCat/Utils/Extensions/Color+extension.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 extension Color {
     init(hex: String) {
-        var color: UInt32 = 0
+        var color: UInt64 = 0
         var r: Double = 0, g: Double = 0, b: Double = 0
-        if Scanner(string: hex.replacingOccurrences(of: "#", with: "")).scanHexInt32(&color) {
+        if Scanner(string: hex.replacingOccurrences(of: "#", with: "")).scanHexInt64(&color) {
             r = Double((color & 0xFF0000) >> 16) / 255.0
             g = Double((color & 0x00FF00) >>  8) / 255.0
             b = Double( color & 0x0000FF       ) / 255.0
