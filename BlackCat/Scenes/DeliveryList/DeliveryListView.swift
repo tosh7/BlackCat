adee943cb0afb84f5bc89bec6e740be97fb8831d
tosh7 <zlia.6.lj.425@gmail.com>
2021-08-06 01:39:50 +0900

    Added ViewModel

--- BlackCat/Scenes/DeliveryList/DeliveryListView.swift
+++ BlackCat/Scenes/DeliveryList/DeliveryListView.swift
@@ -2,17 +2,8 @@ import SwiftUI
 
 struct DeliveryListView: View {
 
-    var columns: [GridItem] = Array(repeating: .init(.fixed(50)), count: 5)
-    // This is a mock data.
-    var tneko: Tneko = Tneko(deriveryList: [
-        Tneko.DeliveryList(deliveryID: 123456, statusList: [
-            DeliveryStatus(status: "輸送中",
-                           date: "6/13",
-                           time: "12:41",
-                           shopName: "羽田クロノゲートベース",
-                           shopID: "032990")
-        ])
-    ])
+    @ObservedObject var viewModel = DeliveryListViewModel()
+    private let columns: [GridItem] = Array(repeating: .init(.fixed(50)), count: 2)
 
     var body: some View {
         NavigationView {
@@ -20,26 +11,17 @@ struct DeliveryListView: View {
                 LazyVGrid(columns: columns,
                           alignment: .center,
                           spacing: 8) {
-                    ForEach((1...tneko.deriveryList.count), id: \.self) { num in
-                        ZStack {
-                            LuggageItemGrid(deliveryStatus: tneko.deriveryList[0].statusList.last!)
+                    if let tneko = viewModel.tneko {
+                        ForEach((0..<tneko.deriveryList.count), id: \.self) { num in
+                            ZStack {
+                                LuggageItemGrid(deliveryStatus: tneko.deriveryList[num].statusList.last!)
+                            }
                         }
                     }
                 }
             }
             .navigationTitle("配達状況一覧")
         }
-        .onAppear(perform: {
-            apiClient.tneko(.init(number01: 429636181995, number02: 398629940844)) { result in
-                switch result {
-                case let .success(tneko):
-//                    self.tneko = tneko
-                    print(tneko)
-                    break
-                case .failure: break
-                }
-            }
-        })
     }
 }
 
--- BlackCat/Scenes/DeliveryList/DeliveryListViewModel.swift
+++ BlackCat/Scenes/DeliveryList/DeliveryListViewModel.swift
@@ -1,6 +1,19 @@
 import Foundation
-import Combine
 
-final class DeliveryListViewModel {
+final class DeliveryListViewModel: ObservableObject {
+    private let goodsIdList: [Int] = [429636181995, 398629940844]
+    @Published var tneko: Tneko?
 
+    init() {
+        apiClient.tneko(.init(number01: 429636181995, number02: 398629940844), completion: { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(tneko):
+                DispatchQueue.main.async {
+                    self.tneko = tneko
+                }
+            case .failure: break
+            }
+        })
+    }
 }
