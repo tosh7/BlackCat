01e22c15555e28e24dfa09e010c6334b33166f7a
tosh7 <zlia.6.lj.425@gmail.com>
2021-08-06 00:13:03 +0900

    added BlackCat's original colors

--- BlackCat/Utils/Extensions/Color+extension.swift
+++ BlackCat/Utils/Extensions/Color+extension.swift
@@ -12,3 +12,27 @@ extension Color {
         self.init(red: r, green: g, blue: b)
     }
 }
+
+extension Color {
+    struct BlackCat {
+        // pure is pastel color
+        static let pureGreen = Color(hex: "0x7fff7f")
+        static let pureRed = Color(hex: "0x#ff7f7f")
+        static let pureBlue = Color(hex: "0x7f7fff")
+        static let purePurple = Color(hex: "0xbf7fff")
+
+        // natural is vivid color
+        static let naturalGreen = Color(hex: "0x00ff00")
+        static let naturalRed = Color(hex: "0xff3333")
+
+        // shadow is monotone color
+        // level's number get larger, color get darker
+        static let shadowLevel1 = Color(hex: "0xe0e0e0")
+        static let shadowLevel2 = Color(hex: "0xc0c0c0")
+        static let shadowLevel3 = Color(hex: "0xa0a0a0")
+        static let shadowLevel4 = Color(hex: "0x808080")
+        static let shadowLevel5 = Color(hex: "0x606060")
+        static let shadowLevel6 = Color(hex: "0x404040")
+        static let shadowLevel7 = Color(hex: "0x202020")
+    }
+}
