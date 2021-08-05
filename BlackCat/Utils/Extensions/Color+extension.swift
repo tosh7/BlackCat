81bc5313d839e05e07d41f83279e2b1dea141643
tosh7 <zlia.6.lj.425@gmail.com>
2021-08-05 23:59:46 +0900

    Added COlor extension

---
+++ BlackCat/Utils/Extensions/Color+extension.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        var color: UInt32 = 0
+        var r: Double = 0, g: Double = 0, b: Double = 0
+        if Scanner(string: hex.replacingOccurrences(of: "#", with: "")).scanHexInt32(&color) {
+            r = Double((color & 0xFF0000) >> 16) / 255.0
+            g = Double((color & 0x00FF00) >>  8) / 255.0
+            b = Double( color & 0x0000FF       ) / 255.0
+        }
+        self.init(red: r, green: g, blue: b)
+    }
+}
