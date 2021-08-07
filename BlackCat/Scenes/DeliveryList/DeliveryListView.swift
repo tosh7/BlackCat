fa42d70af3e561d42dc93f30d34eb4a280bd57a3
tosh7 <zlia.6.lj.425@gmail.com>
2021-08-08 00:12:05 +0900

    Changed base color to black

--- BlackCat/Scenes/DeliveryList/DeliveryListView.swift
+++ BlackCat/Scenes/DeliveryList/DeliveryListView.swift
@@ -5,21 +5,34 @@ struct DeliveryListView: View {
     @ObservedObject var viewModel = DeliveryListViewModel()
     private let columns: [GridItem] = Array(repeating: .init(.fixed(50)), count: 2)
 
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().barTintColor = .black
+    }
+
     var body: some View {
         NavigationView {
-            ScrollView(.vertical) {
-                LazyVGrid(columns: columns,
-                          alignment: .center,
-                          spacing: 8) {
-                    ForEach(viewModel.deliveryList) { deliveryStatus in
-                        LuggageItemGrid(deliveryStatus: deliveryStatus.statusList.last!)
+            ZStack {
+                Color.black
+                    .edgesIgnoringSafeArea(.all)
+
+                ScrollView(.vertical) {
+                    LazyVGrid(columns: columns,
+                              alignment: .center,
+                              spacing: 8) {
+                        ForEach(viewModel.deliveryList) { deliveryStatus in
+                            LuggageItemGrid(deliveryStatus: deliveryStatus.statusList.last!)
+                        }
                     }
                 }
+                .navigationTitle("配達状況一覧")
             }
-            .navigationTitle("配達状況一覧")
         }
+        .preferredColorScheme(.dark)
     }
 }
+
 
 struct DeliveryListView_Previews: PreviewProvider {
     static var previews: some View {
