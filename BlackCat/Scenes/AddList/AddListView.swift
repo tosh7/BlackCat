a3877cd4128cc8147527998c77fc5a9ad84540e3
tosh7 <zlia.6.lj.425@gmail.com>
2021-09-05 18:53:58 +0900

    fixed add view layout

--- BlackCat/Scenes/AddList/AddListView.swift
+++ BlackCat/Scenes/AddList/AddListView.swift
@@ -4,21 +4,45 @@ struct AddListView: View {
     private let viewModel = AddListViewModel()
     @State private var itemNumber: String = ""
     @Environment(\.presentationMode) var presentationMode
+    @State private var showingAlert = false
 
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
 
             VStack(alignment: .center, spacing: 100) {
-                TextField("伝票番号を入力(12桁の数字)", text: $itemNumber)
-                    .padding(.horizontal, 20.0)
+                Text("新しい伝票番号を登録する")
                     .foregroundColor(.white)
+                
+                TextField("    伝票番号を入力(12桁の数字)", text: $itemNumber)
+                    .frame(height: 40)
+                    .foregroundColor(.black)
+                    .background(Color.white)
+                    .padding(.horizontal, 20.0)
+
                 Button(action: {
                     viewModel.input.buttonDidTap(text: itemNumber)
-                    self.presentationMode.wrappedValue.dismiss()
+                    self.showingAlert = true
                 }, label: {
-                    Text("登録する")
+                    ZStack {
+                        Color.BlackCat.naturalGreen.edgesIgnoringSafeArea(.all)
+                            .frame(height: 50, alignment: .center)
+                        Text("登録する")
+                            .foregroundColor(.black)
+                    }
+                    .cornerRadius(10)
+                    .padding(.horizontal, 20)
                 })
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text("登録に失敗しました"),
+                          dismissButton: .default(
+                            Text("OK"),
+                            action: {
+                                self.presentationMode.wrappedValue.dismiss()
+                            }
+                          )
+                    )
+                }
             }
         }
     }
