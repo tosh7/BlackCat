3fe1fe0379d90af3564eb42b9c047900c4987f35
tosh7 <zlia.6.lj.425@gmail.com>
2022-09-21 23:05:04 +0900

    deleted unrelated sources

--- Domain/APIProtcols/RequestType.swift
+++ Domain/APIProtcols/RequestType.swift
@@ -6,17 +6,6 @@ public protocol RequestType {
 }
 
 public extension URLRequest {
-    init<Request>(_ request: Request, baseURL: URL) where Request: RequestType {
-        self.init(url: URL(string: "\(baseURL)/\(type(of: request).path)")!)
-    }
-
-    init?<Request>(_ request: Request, baseURL: URL) where Request: RequestType & Encodable {
-        self.init(url: URL(string: "\(baseURL)/\(type(of: request).path)")!)
-        self.httpMethod = type(of: request).method.rawValue
-        guard let body = try? JSONEncoder().encode(request) else { return nil }
-        httpBody = body
-    }
-
     init?<Request>(_ request: Request, baseURL: URL) where Request: RequestType & URLQueryEncodable {
         guard let queryItems = [String: Int](request) else { return nil }
         let url = URL(string: "\(baseURL)/\(type(of: request).path)")!
