215f533c4957b53e8afbac19445b9aba1a8c494b
tosh7 <zlia.6.lj.425@gmail.com>
2023-05-01 23:42:54 +0900

    Create .vscode

---
+++ BlackCat/Scenes/Setting/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  BlackCat
+//
+//  Created by Satoshi Komatsu on 2023/04/28.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
---
+++ BlackCat/Scenes/Setting/SettingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingViewModel.swift
+//  BlackCat
+//
+//  Created by Satoshi Komatsu on 2023/04/28.
+//
+
+import Foundation
